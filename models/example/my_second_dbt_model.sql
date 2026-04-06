9f0d8eada4682fe82e48eef2491a880ba414f067
vaishnavideshmukh12033 <vaishnavi.deshmukh@hevodata.com>
2026-04-06 13:53:47 +0530

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,9 @@
 
 -- Use the `ref` function to select from other models
-
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+select
+  id,
+  book_name,
+  author,
+  price,
+  price * 0.9 as discounted_price
+from raw_books
